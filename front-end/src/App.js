4136b2ea28e383a34471fe185029ec01328475c5
AntoniJordiNogueraRecasens <toninoguerarecasens@gmail.com>
2021-06-17 16:00:39 +0200

    F

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -12,24 +12,27 @@ import Cups from "./Cups.js";
 import News from "./News.js";
 import Torneo from "./Torneo";
 import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import { Container, Row, Col } from "reactstrap";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <NavTop />
-        <Switch>
-          <Route exact path="/" render={() => <Corepage />} />
-          <Route path="/who" render={() => <WhoIs />} />
-          <Route path="/cover" render={() => <Cover />} />
-          <Route path="/espector" render={() => <ResgistrarEspectador />} />
-          <Route path="/player" render={() => <ResgistrarPlayer />} />
-          <Route path="/login" render={() => <Login />} />
-          <Route path="/contact" render={() => <Contact />} />
-          <Route path="/cups" render={() => <Cups />} />
-          <Route path="/news" render={() => <News />} />
-          <Route path="/torneo" render={() => <Torneo />} />
-        </Switch>
+        <Container className="switchContainer">
+          <Switch>
+            <Route exact path="/" render={() => <Corepage />} />
+            <Route path="/who" render={() => <WhoIs />} />
+            <Route path="/cover" render={() => <Cover />} />
+            <Route path="/espector" render={() => <ResgistrarEspectador />} />
+            <Route path="/player" render={() => <ResgistrarPlayer />} />
+            <Route path="/login" render={() => <Login />} />
+            <Route path="/contact" render={() => <Contact />} />
+            <Route path="/cups" render={() => <Cups />} />
+            <Route path="/news" render={() => <News />} />
+            <Route path="/torneo" render={() => <Torneo />} />
+          </Switch>
+        </Container>
       </BrowserRouter>
     </>
   );
